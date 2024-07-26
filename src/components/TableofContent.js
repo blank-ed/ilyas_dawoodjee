f4ad6901ec08ce811898bac0bdaab6f17e1965e4
blank-ed <ilyas.esack.dawoodjee@gmail.com>
2024-07-26 17:34:30 -0500

    update

---
+++ src/components/TableofContent.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react';
+import './TableofContent.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown, faChevronUp, faTable } from '@fortawesome/free-solid-svg-icons';
+import { Link, useLocation } from 'react-router-dom';
+
+function TableofContent({ contentText }) {
+    const [isOpen, setIsOpen] = useState(true);
+    const [initialLoad, setInitialLoad] = useState(true);
+
+    const toggleCollapse = () => setIsOpen(!isOpen);
+
+    const location = useLocation();
+
+    useEffect(() => {
+        if (!initialLoad && location.hash) {
+            const id = location.hash.replace("#", "");
+            const element = document.getElementById(id);
+            if (element) {
+                const yOffset = -70;
+                const y = element.getBoundingClientRect().top + window.pageYOffset + yOffset;
+                window.scrollTo({ top: y, behavior: 'smooth' });
+            }
+        }
+        setInitialLoad(false);
+    }, [location]);
+
+    return (
+        <div className={`tableofcontentContainer`}>
+            <button className={`tableofcontentTitle`} onClick={toggleCollapse}>
+                <span className="button-title">
+                    <FontAwesomeIcon icon={faTable} />&nbsp;&nbsp;TABLE OF CONTENTS
+                </span>
+                <FontAwesomeIcon icon={isOpen ? faChevronUp : faChevronDown} />
+            </button>
+            <div className={`tableofcontentContent ${isOpen ? 'open' : ''}`}>
+                {contentText.map((value, index) => (
+                    <Link to={`${value.section_link}`} className={`${value.type} tableofcontentsText body`}>
+                        {value.title}
+                    </Link>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default TableofContent;
--- src/pages/BlogPage1.js
+++ src/pages/BlogPage1.js
@@ -5,12 +5,12 @@ import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
 import Collapsable from '../components/Collapsable';
 import CodeBlock from '../components/CodeBlock';
+import TableofContent from '../components/TableofContent';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCalendarDays, faFolderOpen } from '@fortawesome/free-regular-svg-icons';
-import { faPen, faClock, faSection, faBars, faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { faPen, faClock, faSection, faBars, faChevronLeft, faChevronRight, faTag } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 import TestImage from '../components/website_data/test.jpg'
-import { type } from '@testing-library/user-event/dist/type';
 
 function BlogPage1() {
   const pageRef = useRef(null);
@@ -39,6 +39,13 @@ function BlogPage1() {
             <span className="time sub-body"><FontAwesomeIcon icon={faClock} />&nbsp;&nbsp;{timeTaken} minutes</span>
             <span className="folder sub-body"><Link to='/' className='blog_links'><FontAwesomeIcon icon={faFolderOpen} />&nbsp;&nbsp;Folder Name</Link></span>
           </div>
+          <div className="blog_tags">
+            <span className="sub-body"><FontAwesomeIcon className='article_icon' icon={faTag} />&nbsp;</span>
+            <span className="sub-body"><Link className='blog_links' to='/'>computer vision,&nbsp;</Link></span>
+            <span className="sub-body"><Link className='blog_links' to='/'>deep learning,&nbsp;</Link></span>
+            <span className="sub-body"><Link className='blog_links' to='/'>face detection,&nbsp;</Link></span>
+            <span className="sub-body"><Link className='blog_links' to='/'>python</Link></span>
+          </div>
           <img className='Blog_Image' src={TestImage} alt="" />
           <span className="body">MediaPipe<a className='blog_links' href="/" target='_blank'><sup>[1]</sup></a> is an amazing library of ready-to-use deep learning models for common tasks in various domains. <a href="https://www.samproell.io/posts/ai/mediapipe-update-2023/" target='_blank' className='blog_links'>My previous post</a> highlights how you can use it to easily detect facial landmarks. There are many <a href="https://ai.google.dev/edge/mediapipe/solutions/guide#available_solutions" target='_blank' className='blog_links'>other solutions</a> available to explore. In this post however, I want to take a look at another feature, the <a className='blog_links' href="">MediaPipe Model Maker</a>. Model maker allows you to extend the functionality of some MediaPipe solutions by customizing models to your specific use case. With only a few lines of code, you can fine tune models and rewire the internals to accommodate new targets.</span>
           <span className="body">In this particular example, we will customize the hand gesture recognition task to build a model for reading the <a href="https://en.wikipedia.org/wiki/American_manual_alphabet" target='_blank' className='blog_links'>American Sign Language (ASL)</a> fingerspelling alphabet. We are going to learn how a basic solution can be created with small effort. But we will also see that there are limitations to the (current) functionality of the MediaPipe Model Maker.</span>
@@ -49,7 +56,7 @@ function BlogPage1() {
 
         <div className="BlogPage_ContentSection">
           {/* Summary Section */}
-          <span className="section_title"><a className='sectionIcon' href="#summary"><FontAwesomeIcon icon={faSection} /></a>  Summary</span>
+          <span className="section_title sectionIcon" id='summary'><FontAwesomeIcon icon={faSection} />  Summary</span>
           <span className="body">Here is a quick overview of the required steps to customize the gesture recognition task for a new problem. The approach is the same for the more general image classification task.</span>
           <ul>
             <li><strong>Get or create some data.</strong> Image files for each class must be provided in a separate sub folder. The folder names are used as class labels.</li>
@@ -61,21 +68,31 @@ function BlogPage1() {
           </ul>
           <span className="body">Beyond the basic steps, we are going to take a look at the hand gesture embeddings, which MediaPipe uses under the hood and we will also perform a more thorough evaluation of the model’s accuracy.</span>
 
+          {/* Table of Contents */}
+          <TableofContent contentText={[
+            { title: 'Summary', type: 'Title', section_link: '#summary' },
+            { title: 'Prerequisites', type: 'Title', section_link: '#first' },
+            { title: 'Installing MediaPipe Model Maker', type: 'SubTitle', section_link: '#first_first' },
+            { title: 'Getting the data', type: 'SubTitle', section_link: '#first_second' },
+            { title: 'Data preparation', type: 'SubTitle', section_link: '#first_third' },
+            { title: 'References', type: 'Title', section_link: '#references' },
+          ]} />
+
           {/* First Section */}
-          <span className="section_title"><a className='sectionIcon' href="#first"><FontAwesomeIcon icon={faSection} /></a>  Prerequisites</span>
+          <span className="section_title sectionIcon" id='first'><FontAwesomeIcon icon={faSection} />  Prerequisites</span>
           <span className="body">Before getting started with the main code, we need take care of some external resources. First, the Model Maker does not come with the default MediaPipe installation, so we need to install it separately. Next, we also need to get some data to train the ASL alphabet detector. MediaPipe will work directly from the files on disk, so some minor preprocessing on the dataset is also needed.</span>
 
-          <span className="subSection_title"><a className='sectionIcon subSection' href="#first_first"><FontAwesomeIcon icon={faBars} /></a>  Installing MediaPipe Model Maker</span>
+          <span className="subSection_title sectionIcon" id='first_first'><FontAwesomeIcon className='subSection' icon={faBars} />  Installing MediaPipe Model Maker</span>
           <span className="body">Installing is straightforward with pip. Note that the installation might not succeed on Windows. If you are on Windows, you can use the Windows Subsystem for Linux (WSL) instead.</span>
           <CodeBlock title="Shell" extraText={[
             [{ type: 'shell', code: "pip install mediapipe mediapipe-model-maker" }]
           ]} />
 
-          <span className="subSection_title"><a className='sectionIcon subSection' href="#first_second"><FontAwesomeIcon icon={faBars} /></a>  Getting the data</span>
+          <span className="subSection_title sectionIcon" id='first_second'><FontAwesomeIcon className='subSection' icon={faBars} />  Getting the data</span>
           <span className="body">For this example, I am going to use the SigNN Character Database from Kaggle. It contains 8442 images showing 24 characters of the english alphabet. The dataset excludes J and Z, because they are differentiated from other characters through motion (see the image below the post). The dataset was originally created to build a mobile ASL alphabet translator - which basically does what I am creating in this post, only better. The dataset creators have a detailed description of their solution, so definitely check it out and star their page.</span>
           <span className="body">To download the dataset yourself, you need a Kaggle account (which is free). With 1.8GB it is fairly manageable. Before getting started, we will need to do some minor preprocessing, to make it work seamlessly with MediaPipe Model Maker.</span>
 
-          <span className="subSection_title"><a className='sectionIcon subSection' href="#first_third"><FontAwesomeIcon icon={faBars} /></a>  Data preparation</span>
+          <span className="subSection_title sectionIcon" id='first_third'><FontAwesomeIcon className='subSection' icon={faBars} />  Data preparation</span>
           <span className="body">According to the Model Maker documentation, only a small number of training examples is required to retrain/fine tune the models. Approximately 100 examples per class should be sufficient. The easiest way to provide the data to the Model Maker is through a <span className="code">from_folder</span> method. It scans the given folder, interprets any subdirectory as target classes and any containing (image) files as instances of that class. The SigNN dataset is already provided in this format.</span>
           <span className="body">In addition, MediaPipe requires a <span className="code">"none"</span> class for training, which should include examples that do not show any of the target labels. Training will not run without it. We can create an empty folder called <span className="code">"none"</span> within the training directory. This will allow us to run the training, although it would probably be better to provide actual negative examples.</span>
           <span className="body">We could start training with this dataset immediately. But data processing with the Model Maker is quite slow, so I do not want to work with the full dataset. Unfortunately, the MediaPipe interface has no straightforward way to control how much data is used. The easiest way I have found is to simply copy a the desired number of samples to a separate folder on disk. The following script, allows us to extract multiple non-overlapping subsets, including a heldout test set from the original dataset.</span>
@@ -96,7 +113,7 @@ function BlogPage1() {
           ]} />
 
           {/* References */}
-          <span className="section_title reference_section_title"><a className='sectionIcon' href="#references"><FontAwesomeIcon icon={faSection} /></a>  References</span>
+          <span className="section_title reference_section_title sectionIcon" id='references'><FontAwesomeIcon icon={faSection} />  References</span>
           <table className='reference_section_table'>
             <tbody>
               <tr>
